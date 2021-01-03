6c1cbdcdc544c947020b2d0f7fdfc79d8911f853
Reza Amini <aminireza65@gmail.com>
2021-01-03 15:23:31 +0100

    Task 4 updated

--- Task 4/task_4_a.py
+++ Task 4/task_4_a.py
@@ -1,11 +1,19 @@
 from mrjob.job import MRJob
 from mrjob.step import MRStep
 import re
-import datetime
 
 class MRBestCustomers(MRJob):
 
-    def mapper(self, _, line):
+    def mapper_get_quantities(self, _, line):
+        '''
+        This mapper gets each row of the csv files,
+        and then it splits into the columns (cells).
+        Then it checks if the first row is not the header one,
+        and then it yields the quantity of each stock per year
+        :param _: None
+        :param line: one row from the input csv file
+        :return: ((stock, year), quantity)
+        '''
         columns = re.split(r",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", line)
         if not columns[3].isalpha(): # avoid the first row of the data.
             stock = columns[1]
@@ -14,23 +22,31 @@ class MRBestCustomers(MRJob):
             year = match_year.group(0)
             yield ((stock, year), quantity)
 
-    def combiner(self, stock_year, quantity):
-        yield (stock_year, sum(quantity))
-
-    def reducer(self, stock_year, quantity):
+    def reducer_sum_quantities(self, stock_year, quantity):
+        '''
+        This reducer sums the quantity of each stock per each year
+        :param stock_year: stockcode and the year
+        :param quantity: quantity of the stock in that year
+        :return: year, (sum(quantity), stock_year)
+        '''
         stock, year = stock_year
         yield year, (sum(quantity), stock_year)
 
-    def reducer_aggregate(self, _, data):
+    def reducer_find_max_quantity(self, _, data):
+        '''
+        This reducer finds the stock with the highest quantity per each year
+        :param _: year
+        :param data: sum(quantity) of the each stock in that year
+        :return: the stock with the highest quantity in a year
+        '''
         yield max(data)
 
     def steps(self):
         return [
-            MRStep(mapper=self.mapper,
-                   combiner=self.combiner,
-                   reducer=self.reducer),
+            MRStep(mapper=self.mapper_get_quantities,
+                   reducer=self.reducer_sum_quantities),
             MRStep(
-                   reducer=self.reducer_aggregate
+                   reducer=self.reducer_find_max_quantity
             )
         ]
 
--- Task 4/task_4_b.py
+++ Task 4/task_4_b.py
@@ -1,36 +1,54 @@
 from mrjob.job import MRJob
 from mrjob.step import MRStep
 import re
-import datetime
 
 class MRBestCustomers(MRJob):
 
-    def mapper(self, _, line):
+    def mapper_get_amounts(self, _, line):
+        '''
+        This mapper gets each row of the csv files,
+        and then it splits into the columns (cells).
+        Then it checks if the first row is not the header one,
+        and then it yields the amount*quantity of each stock per year
+        :param _: None
+        :param line: one row from the input csv file
+        :return: ((stock, year), totalamount)
+        '''
         columns = re.split(r",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", line)
         if not columns[3].isalpha(): # avoid the first row of the data.
             stock = columns[1]
+            quantity = float(columns[3])
             amount = float(columns[5])
+            totalamount = quantity * amount
             match_year = re.search('\d{4}', columns[4])
             year = match_year.group(0)
-            yield ((stock, year), amount)
+            yield ((stock, year), totalamount)
 
-    def combiner(self, stock_year, amount):
-        yield (stock_year, sum(amount))
-
-    def reducer(self, stock_year, amount):
+    def reducer_sum_amounts(self, stock_year, amount):
+        '''
+        This reducer sums (amount*quantity) of each stock per each year
+        :param stock_year: stockcode and the year
+        :param amount: (amount*quantity) of the stock in that year
+        :return: year, (sum(amount), stock_year)
+        '''
         stock, year = stock_year
         yield year, (sum(amount), stock_year)
 
-    def reducer_aggregate(self, _, data):
+    def reducer_find_max_amount(self, _, data):
+        '''
+        This reducer finds the stock with the highest sum(amount*quantity) per each year
+        :param _: year
+        :param data: sum(amount*quantity) of the each stock in that year
+        :return: the stock with the highest sum(amount*quantity) in a year
+        '''
         yield max(data)
 
     def steps(self):
         return [
-            MRStep(mapper=self.mapper,
-                   combiner=self.combiner,
-                   reducer=self.reducer),
+            MRStep(mapper=self.mapper_get_amounts,
+                   reducer=self.reducer_sum_amounts),
             MRStep(
-                   reducer=self.reducer_aggregate
+                   reducer=self.reducer_find_max_amount
             )
         ]
 
